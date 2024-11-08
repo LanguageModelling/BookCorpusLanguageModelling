78cf9fde98dfbb6f20eafbc7805fe095c64579d8
nljacobson <nljacobs@uvm.edu>
2024-11-08 11:51:39 -0500

    Finished basic byte pair encoder. Horribly inefficient as vocab size grows, will need to modify.

--- Data/BytePair.py
+++ Data/BytePair.py
@@ -1,4 +1,5 @@
 from utils import Indexer
+from collections import Counter
 
 class BytePairEncoder():
     def __init__(self):
@@ -15,30 +16,61 @@ class BytePairEncoder():
             [self.indexer.add_and_get_index(char) for char in sentence]
 
     def encode(self, sentence):
-        start_idx = 0
-        end_idx = 0
-        encoding = []
-        while end_idx < len(sentence):
-            if sentence[start_idx:end_idx] in self.indexer.objs_to_ints.keys():
-                current_word = sentence[start_idx:end_idx]
-                end_idx += 1
-            else:
-                encoding.append(self.indexer.index_of(current_word))
-                start_idx = end_idx
+        encoding = list(sentence)
+        changed = True
+        while changed:
+            changed = False
+            new_encoding = []
+            idx = 0
+            while idx < len(encoding)-1:
+                if encoding[idx] + encoding[idx+1] in self.indexer.objs_to_ints.keys():
+                    new_encoding.append(encoding[idx] + encoding[idx+1])
+                    idx += 2
+                    changed = True
+                else:
+                    new_encoding.append(encoding[idx])
+                    idx += 1
+            new_encoding.append(encoding[-1])
+            encoding = new_encoding
+        return encoding
+                
+
+    def bigramify(self, split_sentence):
+        '''
+        :param: split_sentence: List(str) : sentence encoding into tokens by current encoding method
+        :return: new_corpus : encoding corpus bigrams
+        '''
+        bigrams = [split_sentence[i] + split_sentence[i+1] for i in range(len(split_sentence)-1)]
+        return bigrams
+
 
     def train(self, corpus, vocab_size=1000, char_level = False):
         # Create vocab
         self.add_characters(corpus)
-        while len(self.indexer) < vocab_size:
-            
-
-datalength = 1000
+        while len(self.indexer) < vocab_size and not char_level:
+            bigram_counts = Counter()
+            for sentence in corpus:
+                encoding = self.encode(sentence)
+                bigrams = self.bigramify(encoding)
+                bigram_counts.update(bigrams)
+            new_word = bigram_counts.most_common(1)[0][0]
+            if new_word in self.indexer.objs_to_ints.keys():
+                assert False
+            self.indexer.add_and_get_index(new_word)
+datalength = 1000000
 from Library import Library
 import time
+import matplotlib.pyplot as plt
 lib = Library()
-bpe = BytePairEncoder()
-tic = time.time()
 dataset = lib.get_examples(datalength)
-bpe.train(dataset, 100)
+times = []
+for vocab_size in [100]:
+    bpe = BytePairEncoder()
+    tic = time.time()
+    bpe.train(dataset, vocab_size)
+    times.append(time.time()-tic)
+    print(times[-1])
 
+plt.plot(times)
+plt.show()
     
