06967ff5ba11e24671447c992591fa122f356835
Jainum Sanghavi <jainumsanghavi@Jainums-Laptop.local>
2024-11-24 11:15:03 -0500

    transformer draft 1

---
+++ Transformer/model.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class transformer(nn.module):
+    def __init__(self):
+        print("hello")
+
+    def forward(self,src):
+        print("")
+        return
+    
+
+class PositionalEncoding(nn.module):
+    def __init___(self):
+        print("")
+
+    def forward(self, src):
+        print("")
+
+
+
